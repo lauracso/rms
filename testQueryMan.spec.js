5de283598ce416125cd3066a9032bceac88e28d0
lauracso <laura.egar@cso.ie>
2025-04-04 12:02:41 +0100

    some changes

--- testQueryMan.spec.js
+++ testQueryMan.spec.js
@@ -3,12 +3,6 @@ import { defineConfig, test, expect } from '@playwright/test';
 import { QueryPom } from './poms/pomQuery'; 
 //import { username, password } from '@myhelper/credentials';
 
-export default defineConfig({
-    use: {
-        baseURL: 'https://uat-webserver.cso.ie/RMS/client',
-    },
-});
-
 test('retrieve aer lingus', async({page}) => { 
     const qp =  new QueryPom(page);
     const aerLingus = 'EN00000004';
